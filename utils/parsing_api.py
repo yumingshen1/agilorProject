57c0833f28f63e91f3e524b92579ca8e1a5f8de7
yumingshen <m18515607030@163.com>
2021-12-01 14:47:41 +0800

    点值

--- utils/parsing_api.py
+++ utils/parsing_api.py
@@ -1,15 +1,16 @@
 # -*- coding: gbk -*-
 import requests
 import json
+import xlrd,xlwt
 
 class parsingApi:
-
+    # api首页
     def home_api(self):
         res = requests.get('http://pi.vaiwan.com/piwebapi/')
         data = res.json()['Links']['DataServers']
         str1 = data.split('/')[-1]
         return str1
-
+    # 数据库连接
     def database_list(self):
         da = self.home_api()
         url = 'http://pi.vaiwan.com/piwebapi/'
@@ -18,30 +19,52 @@ class parsingApi:
         data = res_list.json()['Items'][0]['Links']['Points']
         str2 = data.split('dataservers/')[1]
         return str2
-
+    # 所有点信息
     def information(self):
         da = self.database_list()
         url = 'http://pi.vaiwan.com/piwebapi/dataservers/'
         path = f'{url}{da}'
         some_list = requests.get(path)
-        print(type(some_list.json()))
+        # print(type(some_list.json()))
         # print(some_list.json()['Items'])
         name_list = []
         PointType_list = []
+        RecordedData_list = []
         WebId = []
         for i in range(len(some_list.json()['Items'])):
             name_list.append(some_list.json()['Items'][i]['Name'])
-            PointType_list.append(some_list.json()['Items'][i]['PointType'])
-            WebId.append(some_list.json()['Items'][i]['WebId'])
+            name_list.append(some_list.json()['Items'][i]['PointType'])
+            links = (some_list.json()['Items'][i]['Links']['RecordedData'])
+            links = links.split('/streams/')[1]
+            links = f'{"http://pi.vaiwan.com/piwebapi/streams/"}{links}'
+            # print(links)
+            name_list.append(links)
         print(name_list)
-        print(PointType_list)
-        print(WebId)
+        print(type(name_list))
+
+        return name_list
 
 
-
+    # 点的值
     def informationPage(self):
+        da = self.information()
         url = 'http://pi.vaiwan.com/piwebapi/streams/'
 
+
+    # 写入
+    def write_list(self,file_path,datas):
+        f = xlwt.Workbook()
+        sheet1 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)  # 创建sheet
+        # 将数据写入第 i 行，第 j 列
+        i = 0
+        for data in datas:
+            for j in range(len(data)):
+                sheet1.write(i, j, data[j])
+            i = i + 1
+        f.save(file_path)  # 保存文件
+        # for i in range(len(data)):
+        #     print(data[0])
+        #     print(type(data))
 
 
 
@@ -49,3 +72,7 @@ if __name__ == '__main__':
     # parsingApi().home_api()
     # parsingApi().database_list()
     parsingApi().information()
+
+    # data_list = parsingApi().information()
+    # file_path = 'E:/sym/点值.xlsx'
+    # parsingApi().write_list(file_path,data_list)
