d33c722515ca00122c28a09e2ccc80d9dcb56e26
sym <m18515607030@163.com>
2022-03-18 15:02:56 +0800

    html&selenium

---
+++ web/one_selenium.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Time: 2022/3/18 10:22
+# @Author: shenyuming
+
+from selenium import webdriver
+# selenium物理地址：C:\Users\dell\AppData\Local\Programs\Python\Python310\Lib\site-packages\selenium
+# 导入webdriver等同于：from .chrome.webdriver import WebDriver as Chrome
+import time,os
+
+driver = webdriver.Chrome()
+# driver.get('https://www.baidu.com')
+# time.sleep(3)
+# driver.close()
+
+'''
+    通过os模块 工程路径打开本地文件
+'''
+print(__file__)                 #当前文件路径
+html = (os.path.dirname(__file__)) # 工程路径
+html_path = os.path.join(html,'one_html.html')
+# driver.get(html_path)
+
+'''
+    绝对路径打开本地文件
+'''
+# driver.get(r'E:\sym\agilorProject\web\one_html.html')
+
+
+'''
+    利用pathlib库打开本地文件
+'''
+from pathlib import Path
+# path = str(Path('one_html.html').resolve())
+# driver.get(path)
+
+
+'''
+    优雅的打开网页，自动关闭
+'''
+with webdriver.Chrome() as driver:
+    driver.get('https://cn.bing.com')
